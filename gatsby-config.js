5808221f50901b413c3c086a1ffcb661c768c7cf
Eric Elsner <ericelsnercodes@gmail.com>
2022-10-07 14:05:22 -0400

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,41 @@
+require('dotenv').config({
+  path:`.env.${process.env.NODE_ENV}`
+})
+
 module.exports = {
   siteMetadata: {
     title: `ewe-gatsby`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://ericelsner.com`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-image", 
+    "gatsby-plugin-mdx", 
+    "gatsby-plugin-sharp", 
+    "gatsby-transformer-sharp",
+    "gatsby-transformer-remark", 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    }, 
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    { resolve: 'gatsby-source-strapi',
+      options: {
+        apiURL: process.env.STRAPI_API_TOKEN,
+        accessToken: proccess.env.STRAPI_TOKEN,
+        collectionTypes: [],
+        singleTypes: [],
+      },
     },
-    __key: "pages"
-  }]
+  ]
 };
